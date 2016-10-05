2da8be826dd85bc14710aa0b111ed45c7a5a4d5c
Andrew MacLeay <amacleay@athenahealth.com>
2016-10-04 23:44:19 -0400

    Fix lint, formatting

--- src/containers/OtherAppShell.jsx
+++ src/containers/OtherAppShell.jsx
@@ -3,12 +3,12 @@ import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 
 const OtherAppShell = ({ children }) => (
-	<div>
-	  <h1> Super Cool App part Dos </h1>
-	  { children }
-	  <Link to="/">Root</Link>
-	</div>
-	);
+    <div>
+      <h1> Super Cool App part Dos </h1>
+      { children }
+      <Link to="/">Root</Link>
+    </div>
+    );
 
 OtherAppShell.propTypes = {
   children: PropTypes.element,
